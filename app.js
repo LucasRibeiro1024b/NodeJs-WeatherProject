0ed6e41497ad54435824048f787629cbd32f73f8
RibeiroLucas <lucasribeiro1024b@gmail.com>
2022-05-14 10:19:26 -0300

    work: HTTPS.get().

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
+
+app.get('/', (req, res)=>{
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=b0faa5c6ae23afdc9dc7c0dbb85ad472";
+
+  https.get(url, function(response){
+    console.log(response);
+  });
+
+  res.send("Server up and running.");
+});
 
 app.listen(3000, ()=>{
   console.log("Server is running on port 3000.");
